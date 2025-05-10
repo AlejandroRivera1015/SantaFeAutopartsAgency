fadfcc405e76260b10d7cb890eee152926ff8a01
AlexDev <ariverar151097@icloud.com>
2025-05-09 21:22:12 -0600

    Item Object

---
+++ src/main/java/com/autoparts/SantaFeCarsAgency/DTO/Item.java
@@ -0,0 +1,17 @@
+package com.autoparts.SantaFeCarsAgency.DTO;
+
+
+import com.autoparts.SantaFeCarsAgency.Entity.Product;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Embeddable
+public class Item {
+    private Product productId;
+    private Long quantity;
+}
