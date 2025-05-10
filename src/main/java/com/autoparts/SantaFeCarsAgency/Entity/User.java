423bcce919ce6b588d06dd70a48d4d013f5839a1
AlexDev <ariverar151097@icloud.com>
2025-05-09 21:23:35 -0600

    User fn methods , and Order relation declaration

--- src/main/java/com/autoparts/SantaFeCarsAgency/Entity/User.java
+++ src/main/java/com/autoparts/SantaFeCarsAgency/Entity/User.java
@@ -1,16 +1,28 @@
 package com.autoparts.SantaFeCarsAgency.Entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
 
 @Entity
 @Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String  name;
     private String email;
     private String password;
     private String phone;
     private String address;
-    private Double numbe;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders;
 
 }
