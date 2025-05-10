b4b81e9edfd7f4f3da8171d5ca711e717365bfb7
AlexDev <ariverar151097@icloud.com>
2025-05-09 21:22:38 -0600

    Product Entity

---
+++ src/main/java/com/autoparts/SantaFeCarsAgency/Entity/Product.java
@@ -0,0 +1,24 @@
+package com.autoparts.SantaFeCarsAgency.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String category;
+    private  Double price;
+    private String seller;
+    private  Long quantity;
+}
