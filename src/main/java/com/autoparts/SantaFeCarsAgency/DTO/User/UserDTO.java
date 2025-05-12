947a2d69474706c2650942c045d518f5631ae510
AlexDev <ariverar151097@icloud.com>
2025-05-12 17:21:10 -0600

    User DTO for app consume

---
+++ src/main/java/com/autoparts/SantaFeCarsAgency/DTO/User/UserDTO.java
@@ -0,0 +1,12 @@
+package com.autoparts.SantaFeCarsAgency.DTO.User;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String name;
+}
