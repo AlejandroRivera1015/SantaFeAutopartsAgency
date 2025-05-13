ea638e0c1832f762c7befadb5aa492ded4e7cf1a
AlexDev <ariverar151097@icloud.com>
2025-05-12 22:48:59 -0600

    jwt creation for handle login

--- src/main/java/com/autoparts/SantaFeCarsAgency/Controller/AuthController.java
+++ src/main/java/com/autoparts/SantaFeCarsAgency/Controller/AuthController.java
@@ -2,11 +2,18 @@ package com.autoparts.SantaFeCarsAgency.Controller;
 
 import com.autoparts.SantaFeCarsAgency.DTO.User.UserDTO;
 import com.autoparts.SantaFeCarsAgency.Entity.User;
+import com.autoparts.SantaFeCarsAgency.Security.Utils.JwtUtil;
+import com.autoparts.SantaFeCarsAgency.Service.Config.CookiesService;
 import com.autoparts.SantaFeCarsAgency.Service.User.UserServiceImpl;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("auth")
@@ -16,11 +23,25 @@ public class AuthController {
     @Autowired
     UserServiceImpl userService;
 
+    @Autowired
+    CookiesService cookiesService;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
     @PostMapping("login")
-    public ResponseEntity<?> logIn(@RequestBody User user){
+    public ResponseEntity<?> logIn(@RequestBody User user, HttpServletResponse response){
         UserDTO userResponse = userService.login(user.getEmail(), user.getPassword());
 
         if (userResponse != null  && userResponse.getId() != 0L ) {
+            Map<String,String> claims = new HashMap<>();
+            claims.put("id", userResponse.getId().toString());
+            claims.put("name",userResponse.getName().toString());
+            claims.put("role", String.valueOf(userResponse.getRole()));
+            String jwtToken = jwtUtil.generateToken(claims);
+            System.out.println("JWT Token: " + jwtToken);
+            this.cookiesService.setJWTCookie(jwtToken, response);
+
             return new ResponseEntity<>(userResponse, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
