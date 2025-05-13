a77d1abad6098a24573e63088355c20f1436217f
AlexDev <ariverar151097@icloud.com>
2025-05-12 22:55:49 -0600

    User dto, adding role for response

--- src/main/java/com/autoparts/SantaFeCarsAgency/DTO/User/UserDTO.java
+++ src/main/java/com/autoparts/SantaFeCarsAgency/DTO/User/UserDTO.java
@@ -4,9 +4,12 @@ package com.autoparts.SantaFeCarsAgency.DTO.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 public class UserDTO {
     private Long id;
     private String name;
+    private char role;
 }
