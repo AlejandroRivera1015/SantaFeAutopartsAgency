bb6849fd58bd68184ec4f2e09aad74a5d8b601fa
AlexDev <ariverar151097@icloud.com>
2025-05-17 17:53:44 -0600

    UserDTO for handling requests

--- src/main/java/com/autoparts/SantaFeCarsAgency/DTO/User/UserDTO.java
+++ src/main/java/com/autoparts/SantaFeCarsAgency/DTO/User/UserDTO.java
@@ -3,11 +3,13 @@ package com.autoparts.SantaFeCarsAgency.DTO.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class UserDTO {
     private Long id;
     private String name;
