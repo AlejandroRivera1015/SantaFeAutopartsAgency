13223a5cfac368abe4ddd2da92ba46351f92a25a
AlexDev <ariverar151097@icloud.com>
2025-05-17 17:52:53 -0600

    OrderDTO for handling requests

---
+++ src/main/java/com/autoparts/SantaFeCarsAgency/DTO/Order/OrderDTO.java
@@ -0,0 +1,18 @@
+package com.autoparts.SantaFeCarsAgency.DTO.Order;
+
+
+import com.autoparts.SantaFeCarsAgency.DTO.CartDTO;
+import com.autoparts.SantaFeCarsAgency.DTO.User.UserDTO;
+import com.autoparts.SantaFeCarsAgency.Entity.Cart;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+@Getter
+public class OrderDTO {
+    private UserDTO user;
+    private CartDTO cart;
+
+}
