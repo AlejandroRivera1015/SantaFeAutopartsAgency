371225166e29cdce87f8144ea70e75a3ee61063e
AlexDev <ariverar151097@icloud.com>
2025-05-20 12:59:36 -0600

    change cart typo on OrderDTO

--- src/main/java/com/autoparts/SantaFeCarsAgency/DTO/Order/OrderDTO.java
+++ src/main/java/com/autoparts/SantaFeCarsAgency/DTO/Order/OrderDTO.java
@@ -1,18 +1,19 @@
 package com.autoparts.SantaFeCarsAgency.DTO.Order;
 
-
-import com.autoparts.SantaFeCarsAgency.DTO.CartDTO;
+import com.autoparts.SantaFeCarsAgency.DTO.Item.Item;
 import com.autoparts.SantaFeCarsAgency.DTO.User.UserDTO;
 import com.autoparts.SantaFeCarsAgency.Entity.Cart;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.List;
+
 @ToString
 @AllArgsConstructor
 @Getter
 public class OrderDTO {
     private UserDTO user;
-    private CartDTO cart;
+    private List<Item> cart;
 
 }
