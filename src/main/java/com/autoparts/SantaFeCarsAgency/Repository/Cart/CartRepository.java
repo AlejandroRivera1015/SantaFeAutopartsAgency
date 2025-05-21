3bef4886c3ecd8655a6b6b104584ef5ef017620c
AlexDev <ariverar151097@icloud.com>
2025-05-20 23:17:37 -0600

    CartRepository creation, for jpa Cart methods

---
+++ src/main/java/com/autoparts/SantaFeCarsAgency/Repository/Cart/CartRepository.java
@@ -0,0 +1,8 @@
+package com.autoparts.SantaFeCarsAgency.Repository.Cart;
+
+import com.autoparts.SantaFeCarsAgency.Entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart,Long> {
+
+}
