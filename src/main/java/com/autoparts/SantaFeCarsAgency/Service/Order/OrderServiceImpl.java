e75d42c4e0e09fb522b9103772f03cdaadc3940a
AlexDev <ariverar151097@icloud.com>
2025-05-20 21:06:40 -0600

    order Service for handling request creation, first step of the Order lifecycle.
    add NoAvailable y OutStock exceptions

---
+++ src/main/java/com/autoparts/SantaFeCarsAgency/Service/Order/OrderServiceImpl.java
@@ -0,0 +1,82 @@
+package com.autoparts.SantaFeCarsAgency.Service.Order;
+
+import com.autoparts.SantaFeCarsAgency.DTO.Item.Item;
+import com.autoparts.SantaFeCarsAgency.DTO.Order.OrderDTO;
+import com.autoparts.SantaFeCarsAgency.Entity.Cart;
+import com.autoparts.SantaFeCarsAgency.Entity.Order;
+import com.autoparts.SantaFeCarsAgency.Entity.User;
+import com.autoparts.SantaFeCarsAgency.Exceptions.Product.AvailableProductException;
+import com.autoparts.SantaFeCarsAgency.Exceptions.Product.OutOfStockException;
+import com.autoparts.SantaFeCarsAgency.Repository.Order.OrderRepository;
+import com.autoparts.SantaFeCarsAgency.Repository.User.UserRepository;
+import com.autoparts.SantaFeCarsAgency.Service.Product.ProductServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ProductServiceImpl productService;
+
+    @Override
+    public Order createOrderService(OrderDTO orderResquest){
+        Order order = new Order();
+        Optional<User> isUser = userRepository.findById(orderResquest.getUser().getId());
+
+        try {
+            if(isUser.isPresent()) {
+                List<Item> orderRequestCart = orderResquest.getCart();
+                for (Item item : orderRequestCart) {
+                    if (!productService.isProductAvailable(item.getProductId())) {
+                        throw new OutOfStockException("out of stock", item.getProductId());
+                    }
+                    if (!productService.stockAvailable(item.getProductId(), item.getQuantity())) {
+                        throw new AvailableProductException("no available item", item.getProductId());
+                    }
+                }
+                order.setUser(isUser.get());
+                Cart cart = new Cart();
+                cart.setItems(orderRequestCart);
+                cart.setOrder(order);
+                order.setCart(cart);
+                order.setIsReady(false);
+                order.setOrderDate(LocalDateTime.now());
+                order.setOrderStatus("created");
+                //TODO : add save order
+                return order;
+
+
+            }
+        }catch (AvailableProductException | OutOfStockException e){
+            order.setUser(isUser.get());
+            order.setOrderDate(LocalDateTime.now());
+            order.setIsReady(false);
+
+
+            if(e instanceof AvailableProductException){
+                order.setOrderStatus("outStockItems");
+           }
+           if(e instanceof  OutOfStockException){
+                order.setOrderStatus("noAvailableItems");
+           }
+
+        }
+        finally {
+            return  order;
+        }
+
+    }
+
+}
