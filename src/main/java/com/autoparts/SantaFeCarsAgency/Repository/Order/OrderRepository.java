a3f7707b37ba7314dac0ab0d4869c60e669ffc90
Alex Dev <159105092+AlejandroRivera1015@users.noreply.github.com>
2025-05-25 19:25:12 -0600

    Revert "Get all orders ft"

--- src/main/java/com/autoparts/SantaFeCarsAgency/Repository/Order/OrderRepository.java
+++ src/main/java/com/autoparts/SantaFeCarsAgency/Repository/Order/OrderRepository.java
@@ -3,14 +3,10 @@ package com.autoparts.SantaFeCarsAgency.Repository.Order;
 import com.autoparts.SantaFeCarsAgency.Entity.Order;
 import com.autoparts.SantaFeCarsAgency.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
-    List<Order> findByUser(User user);
-    List<Order> findByOrderDate(LocalDateTime orderDate);
+    Optional<Order> findByUser(User user);
 }
